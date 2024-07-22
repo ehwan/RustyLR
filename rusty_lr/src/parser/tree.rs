b1211109c334e8c372e414b2f213adb7865a170b
Taehwan Kim <ehwank98@gmail.com>
2024-07-22 14:48:58 +0900

    add range() to Tree, TreeStr and move begin(), end() to pub(crate)

--- rusty_lr/src/parser/tree.rs
+++ rusty_lr/src/parser/tree.rs
@@ -10,18 +10,22 @@ pub enum Tree {
 impl Tree {
     /// get the slice of input sequence that this tree represents
     pub fn slice<'a, Term>(&self, input: &'a [Term]) -> &'a [Term] {
-        &input[self.begin()..self.end()]
+        &input[self.range()]
+    }
+    /// get the range on input sequence that this tree represents
+    pub fn range(&self) -> std::ops::Range<usize> {
+        self.begin()..self.end()
     }
 
     /// get the begin index of input sequence that this tree represents
-    pub fn begin(&self) -> usize {
+    pub(crate) fn begin(&self) -> usize {
         match self {
             Tree::Terminal(idx) => *idx,
             Tree::NonTerminal(_, children) => children.first().map(|x| x.begin()).unwrap_or(0),
         }
     }
     /// get the end index of input sequence that this tree represents
-    pub fn end(&self) -> usize {
+    pub(crate) fn end(&self) -> usize {
         match self {
             Tree::Terminal(idx) => *idx + 1,
             Tree::NonTerminal(_, children) => children.last().map(|x| x.end()).unwrap_or(0),
@@ -41,18 +45,22 @@ pub enum TreeStr {
 impl TreeStr {
     /// get the slice of input sequence that this tree represents
     pub fn str<'a>(&self, input: &'a str) -> &'a str {
-        &input[self.begin()..self.end()]
+        &input[self.range()]
+    }
+    /// get the range on input sequence that this tree represents
+    pub fn range(&self) -> std::ops::Range<usize> {
+        self.begin()..self.end()
     }
 
     /// get the begin index of input sequence that this tree represents
-    pub fn begin(&self) -> usize {
+    pub(crate) fn begin(&self) -> usize {
         match self {
             TreeStr::Terminal(beg, _end) => *beg,
             TreeStr::NonTerminal(_, children) => children.first().map(|x| x.begin()).unwrap_or(0),
         }
     }
     /// get the end index of input sequence that this tree represents
-    pub fn end(&self) -> usize {
+    pub(crate) fn end(&self) -> usize {
         match self {
             TreeStr::Terminal(_beg, end) => *end,
             TreeStr::NonTerminal(_, children) => children.last().map(|x| x.end()).unwrap_or(0),
