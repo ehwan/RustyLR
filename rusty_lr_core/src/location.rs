cde85bf2550c46db86570991d8b958c2cd5634b8
Taehwan Kim <ehwank98@gmail.com>
2025-06-29 00:54:05 +0900

    location

--- rusty_lr_core/src/location.rs
+++ rusty_lr_core/src/location.rs
@@ -1,5 +1,8 @@
 /// trait for token location in the input stream
-pub trait Location {
+pub trait Location: Clone + Default {
     /// returns the smallest range that covers both locations
     fn merge(self, other: Self) -> Self;
+
+    /// create a new zero-length location that is next to the current location
+    fn next_zero(&self) -> Self;
 }
