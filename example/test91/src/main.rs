830162c3e06e76b198ca54620b297671ddd88787
Taehwan Kim <tthhkimgo@gmail.com>
2026-06-30 09:21:43 +0900

    Revert "test cases"
    This reverts commit 8a38224671f0f66e18b427453f97be1c9c0d580f.

--- example/test91/src/main.rs
+++
@@ -1,9 +0,0 @@
-mod parser;
-
-fn main() {
-    let mut context = parser::ExprContext::with_default_userdata();
-    context.feed('t').unwrap();
-
-    let parses: Vec<_> = context.accept_all().unwrap().collect();
-    println!("accepted {parses:#?}");
-}
