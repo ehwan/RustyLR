dc5699b365d5073285d84fe9d2aee94804c048ac
Taehwan Kim <tthhkimgo@gmail.com>
2026-07-25 07:39:00 +0900

    Release: Bump versions and execute cargo publish
    Version updates:
    - rusty_lr: 4.6.0 -> 4.7.0
    - rusty_lr_buildscript: 0.71.0 -> 0.72.0
    - rustylr: 1.37.0 -> 1.38.0
    - rusty_lr_core: 4.6.0 -> 4.7.0
    - rusty_lr_parser: 4.6.0 -> 4.7.0
    - rusty_lr_derive: 2.52.0 -> 2.53.0
    - vscode extension: 1.37.0 -> 1.38.0

--- scripts/diff/calculator.rs
+++ scripts/diff/calculator.rs
@@ -728,15 +728,15 @@ impl ::rusty_lr::parser::Parser for Parser {
     }
     #[doc(hidden)]
     fn __rusty_lr_parser_version() -> (usize, usize, usize) {
-        (4, 6, 0)
+        (4, 7, 0)
     }
     #[doc(hidden)]
     fn __rustylr_version() -> (usize, usize, usize) {
-        (1, 37, 0)
+        (1, 38, 0)
     }
     #[doc(hidden)]
     fn __rusty_lr_version() -> (usize, usize, usize) {
-        (4, 6, 0)
+        (4, 7, 0)
     }
 }
 
